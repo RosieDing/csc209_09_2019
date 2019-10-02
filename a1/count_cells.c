34bd63a6b9947070c62a4c2d4e9855aceb23e707
dingruos <Drose@2015>
2019-10-02 04:36:35 -0400

    add count cell

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read_image(int num_rows, int num_cols, 
-                int arr[num_rows][num_cols], FILE *fp);
+void read_image( int num_rows, int num_cols, int arr[num_rows][num_cols], FILE *fp);
                 
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
+int count_cells(int num_rows, int num_cols,int arr[num_rows][num_cols]);
 // Remember to include the function prototype for count_cells
 
 
 int main(int argc, char **argv) {
     // Print a message to stderr and exit with an argument of 1 if there are
     // not the right number of parameters, or the second argument is not -p
+    FILE *file;
+    file = fopen("sample", "r");
+    //file = fopen("A2.txt", "r");
 
+    if (file == NULL)
+    {
+    	exit(1);
+    }
+
+    int num_rows, num_cols;
+
+    fscanf(file, "%d", &num_rows);
+    printf("%d\n", num_rows);
+    fscanf(file, "%d", &num_cols);
+    printf("%d\n", num_cols);
+
+    int array[num_rows][num_cols];
+
+    read_image(num_rows, num_cols, array, file);
+    print_image(num_rows, num_cols, array);
+
+    printf("%s\n","show digits");
+    printf("%d\n", count_cells(num_rows, num_cols, array));
+    fclose(file);
+    
 
     return 0;
+
 }
