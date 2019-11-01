70fc6a39f329a25744cedc3305cb59453c17ea48
dingruos <Drose@2015>
2019-11-01 18:29:16 -0400

    edit

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -30,7 +30,59 @@ int main(void) {
       exit(1);
   }
   
-  // TODO
+  int pipe_fd[2];
+  if(pipe(pipe_fd) == -1){
+    perror("pipe failed");
+    exit(1);
+  }
+
+  int pid = fork();
+  if(pid < 0 ){
+    perror("fork");
+    exit(1);
+  }
+  if(pid > 0){//parent process
+    
+    if(close(pipe_fd[0])== -1){
+      perror("parent read");
+      exit(1);
+    }
+    write(pipe_fd[1], user_id, 10);
+    write(pipe_fd[1], password, 10);
+
+    int status;
+        wait(&status);
+        if (WIFEXITED(status)){
+            if (WEXITSTATUS(status) == 0){
+                printf(SUCCESS);
+            } else if (WEXITSTATUS(status) == 2) {
+                printf(INVALID);
+            } else if (WEXITSTATUS(status) == 3) {
+                printf(NO_USER);
+            }
+        }
+
+  }
+
+  if(pid == 0){
+    if(close(pipe_fd[1])== -1){
+      perror("child write");
+      exit(1);
+    }
+    if (dup2(pipe_fd[0], fileno(stdin)) == -1) {
+      perror("child dup2");
+      exit(1);
+    }
+
+    if (close(fd[0]) == -1) {
+            perror("child close");
+            exit(1);
+    } 
+    execl("./validate", "validate", NULL);
+
+
+
+  }
 
   return 0;
 }
