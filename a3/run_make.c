398f1e4664aba9472f2b7b81ccabf13522d431b6
dingruos <Drose@2015>
2019-11-13 18:16:54 -0500

    add runmake

--- a3/run_make.c
+++ a3/run_make.c
@@ -8,9 +8,23 @@
 
 #include "pmake.h"
 
+extern Rule **record_rules;
+extern int length_record_rules;
 
 void run_make(char *target, Rule *rules, int pflag) {
-    // TODO
+	if(record_rules == NULL || length_record_rules ==0){
+		return 0;
+	}
+    //1. search the rule for the given target
+    Rule* first_rule = serach_rules(record_rules, length_record_rules, target);
+    //the selected target is not in the list
+    if(first_rule == NULL){
+    	first_rule = rules;
+    }
+
+    //
+
+
     return ;
 }
 
